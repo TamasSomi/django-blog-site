37b7f1cd35d205172adc125836495fd6a6699771
TamasSomi <>
2023-09-03 15:16:15 +0000

    debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = [
     'TamasSomi.herokuapp.com',
     '127.0.0.1',
     'localhost',
-    'django-blog-site-ts-2982e7d63c26.herokuapp.com'
+    'django-blog-site-ts-2982e7d63c26.herokuapp.com',
+    ' * '
 ]
 
 
