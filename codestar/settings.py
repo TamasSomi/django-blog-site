c8816469119f24ec25ae126ac176d665fd4b8be3
TamasSomi <>
2023-09-04 15:09:09 +0000

    add templates and urls

--- codestar/settings.py
+++ codestar/settings.py
@@ -57,9 +57,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'blog',
 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
